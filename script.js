901c88fdc70c7cd1c87015deaae339dfbd59f992
Rita Santos <rita.salgado.santos@gmail.com>
2021-04-09 22:03:13 +0100

    change class structure and add example functionality

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let canvas = document.getElementById("game");
 let ctx = canvas.getContext("2d");
 
+let lastDate = Date.now();
+
 function notBlurry() {
     ctx.mozImageSmoothingEnabled = false;
     ctx.webkitImageSmoothingEnabled = false;
@@ -8,60 +10,134 @@ function notBlurry() {
     ctx.imageSmoothingEnabled = false;
 }
 
-class Button {
-    constructor(imageNormal, imagePressed, scale) {
-        this.isPressed = false;
-        this.imageNormal = new Image();
-        this.imageNormalLoaded = false;
-        this.imageNormal.addEventListener('load', () => {
-            this.imageNormalLoaded = true;
-            this.width = scale * this.imageNormal.naturalWidth;
-            this.height =  scale * this.imageNormal.naturalHeight;
+class Entity {
+    constructor(image, x, y, scale) {
+        this.x = x;
+        this.y = y;
+        this.image = new Image();
+        this.imageLoaded = false;
+        this.image.addEventListener('load', () => {
+            this.imageLoaded = true;
+            this.width = scale * this.image.naturalWidth;
+            this.height = scale * this.image.naturalHeight;
         }, false);
-        this.imageNormal.src = imageNormal;
+        this.image.src = image;
+    }
 
-        this.imagePressed = new Image ();
+    getCanvasPos() {
+        return {
+            x: ctx.canvas.width / 2 - this.width / 2 + this.x,
+            y: ctx.canvas.height / 2 - this.height / 2 + this.y
+        };
+    }
+
+    draw(ctx) {
+        notBlurry();
+        if (this.imageLoaded) {
+            let canvasPos = this.getCanvasPos();
+            ctx.drawImage(this.image, canvasPos.x, canvasPos.y, this.width, this.height);
+        }
+    }
+}
+
+class Button extends Entity {
+    constructor(imageNormal, imagePressed, x, y, scale) {
+        super(imageNormal, x, y, scale);
+        this.isPressed = false;
+        this.imagePressed = new Image();
         this.imagePressedLoaded = false;
         this.imagePressed.addEventListener('load', () => {
             this.imagePressedLoaded = true;
         }, false);
         this.imagePressed.src = imagePressed;
     }
-    
-    draw(ctx) {
-        notBlurry();
-        if (this.isPressed ? this.imagePressedLoaded : this.imageNormalLoaded) {
-            ctx.drawImage(this.isPressed ? this.imagePressed : this.imageNormal, ctx.canvas.width/2 - this.width/2, ctx.canvas.height/2 - this.height/2, this.width, this.height);
-        } 
+
+    handle(event, action) {
+        if (event.x > ctx.canvas.width / 2 - this.width / 2 && event.x < ctx.canvas.width / 2 + this.width / 2
+            && event.y > ctx.canvas.height / 2 - this.height / 2 && event.y < ctx.canvas.height / 2 + this.height / 2) {
+            if (event.type == 'mousedown') {
+                this.isPressed = true;
+            } else if (event.type == 'mouseup') {
+                action();
+            }
+        }
     }
 
-    handle(event) {
-        if (event.x > ctx.canvas.width/2 - this.width/2 && event.x < ctx.canvas.width/2 + this.width/2
-            && event.y > ctx.canvas.height/2 - this.height/2 && event.y < ctx.canvas.height/2 + this.height/2) {
-                if (event.type == 'mousedown') {
-                    this.isPressed = true;
-                } else if (event.type == 'mouseup') {
-                    this.isPressed = false;
-                }
-            }
+    draw(ctx) {
+        notBlurry();
+        if (this.isPressed ? this.imagePressedLoaded : this.imageLoaded) {
+            let canvasPos = this.getCanvasPos();
+            ctx.drawImage(this.isPressed ? this.imagePressed : this.image, canvasPos.x, canvasPos.y, this.width, this.height);
+        }
     }
 }
 
-let start = new Button('images/red_start_button_normal.png', 'images/red_start_button_pressed.png', 8);
+class MovingObject extends Entity {
+    constructor(image, x, y, sx, sy, scale) {
+        super(image, x, y, scale);
+        this.sx = sx;
+        this.sy = sy;
+    }
 
-canvas.addEventListener('mousedown', event => {
-    start.handle(event);
-});
+    update(delta_time) {
+        let test_x = this.x + delta_time * this.sx;
+        let test_y = this.y + delta_time * this.sy;
 
-canvas.addEventListener('mouseup', event => {
-    start.handle(event);
-});
+        if (test_x - this.width / 2 <= -ctx.canvas.width / 2 || test_x + this.width / 2 >= ctx.canvas.width / 2) {
+            this.sx = -this.sx;
+        }
+        if (test_y - this.height / 2 <= -ctx.canvas.height / 2 || test_y + this.height / 2 >= ctx.canvas.height / 2) {
+            this.sy = -this.sy;
+        }
+
+        this.x += delta_time * this.sx;
+        this.y += delta_time * this.sy;
+        console.log(this.x);
+    }
+}
+
+class Background extends Entity {
+    constructor(image, x, y, scale) {
+        super(image, x, y, scale);
+    }
+}
+
+let startButton = new Button('images/red_start_button_normal.png', 'images/red_start_button_pressed.png', 0, 0, 10);
+let banana = new MovingObject('images/banana1.png', 0, 0, 0.1, 0.5, 7);
+let gorilla = new Background('images/gorilla4.png', 0, 0, 6);
+
+for (let e of ['mousedown', 'mouseup']) {
+    canvas.addEventListener(e, event => {
+        startButton.handle(event, startGame);
+    });
+}
+
+let cenas = [startButton];
 
 function draw() {
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
-    start.draw(ctx);
-    requestAnimationFrame(draw);
+
+    for (let e of cenas) {
+        e.draw(ctx);
+    }
 }
 
-draw();
+function startGame() {
+    cenas = [gorilla, banana];
+}
+
+function animate() {
+    let currentDate = Date.now();
+    let delta_time = currentDate - lastDate;
+    lastDate = currentDate;
+    for (let e of cenas) {
+        if (e instanceof MovingObject) {
+            e.update(delta_time);
+        }
+        draw();
+    }
+    requestAnimationFrame(animate);
+}
+
+animate();
